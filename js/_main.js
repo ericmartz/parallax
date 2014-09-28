c93d241131bd484f615e96ab09cf6d4364043902
Eric Martz <ericmartz@gmail.com>
2014-09-28 17:11:51 -0400

    Deleted elements within the sections of index.html to create my own parallax stuff

--- js/_main.js
+++ js/_main.js
@@ -31,7 +31,9 @@
 	function adjustWindow(){
 		
 		// Init Skrollr
-		var s = skrollr.init();
+		var s = skrollr.init({
+			forceHeight: false,
+		});
 		
 		// Get window size
 	    winH = $window.height();
